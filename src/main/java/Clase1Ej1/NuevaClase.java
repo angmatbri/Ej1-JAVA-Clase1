d539d0b008b0b9a4bf705ae61d5e9e77351ec4a0
angmatbri <79180100+angmatbri@users.noreply.github.com>
2021-02-16 23:25:44 -0300

    Update NuevaClase.java

--- src/main/java/Clase1Ej1/NuevaClase.java
+++ src/main/java/Clase1Ej1/NuevaClase.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clase1Ej1;
 
-/**
- *
- * @author profmatias
- */
 public class NuevaClase {
     
 }
